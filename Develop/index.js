1de02ad1d397e42ab0bcdb622d7c0ab4ec29e572
Devon <devonross50@yahoo.com>
2021-05-13 19:28:24 -0500

    updated comments and reformated README file

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,9 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
+//An array of questions for user input
 const questions = () =>{
     return inquirer.prompt([
         {
